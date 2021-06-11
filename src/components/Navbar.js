e7de757ac8f5c920135f2b65d7234e2c11a5a5b5
JongWoo Ha <jongwooha98@gmail.com>
2021-06-11 23:59:44 +0900

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,8 @@ import './Navbar.scss';
 import logo from '../assets/logo.png';
 
 function Navbar() {
-  // const [expanded, setExpanded] = useState('');
   return (
-    <nav className="navbar navbar-expand-md sticky-top navbar-dark ">
+    <nav className="navbar">
       <div className="navbar__container container-fluid">
         <Link className="navbar-brand" to="/">
           NEW HEIM
@@ -24,46 +23,18 @@ function Navbar() {
           <i class="toggler-icon fas fa-bars" />
         </button>
 
-        <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbarToggler"
-        >
-          <ul className="navbar-nav mr-auto">
+        <div className="collapse navbar-collapse" id="navbarToggler">
+          <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link" to="/">
                 Home
               </Link>
             </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggler"
-                role="button"
-                id="dropdownMenuLink"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                About
-              </a>
-
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <li>
-                  <Link className="dropdown-item" to="/team">
-                    Who we are
-                  </Link>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/team">
-                    What we offer
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="team">
-                    Meet the team
-                  </a>
-                </li>
-              </ul>
+            <li className="nav-item">
+              <Link className="nav-link" to="/team">
+                Who we are
+              </Link>
             </li>
-
             <li className="nav-item">
               <Link className="nav-link" to="/pricing">
                 Pricing
